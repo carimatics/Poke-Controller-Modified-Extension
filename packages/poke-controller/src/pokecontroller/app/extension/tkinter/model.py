65ce24405fe19b979c7a8ee2086492dddbe04e5f
carimatics <github.carimatics@gmail.com>
2025-11-24 10:11:42 +0900

    refactor: implement abstract methods and extend `PokeControllerExtensionAppModel` with concrete method definitions for improved functionality

--- packages/poke-controller/src/pokecontroller/app/extension/tkinter/model.py
+++ packages/poke-controller/src/pokecontroller/app/extension/tkinter/model.py
@@ -3,4 +3,180 @@ from ...model import (
 )
 
 class PokeControllerExtensionAppModel(PokeControllerAppModel):
-    pass
+    def load_commands(self):
+        pass
+
+    def start_command(self):
+        pass
+
+    def stop_command(self):
+        pass
+
+    def pause_command(self):
+        pass
+
+    def set_command_shortcut_number(self):
+        pass
+
+    def register_command_shortcut(self):
+        pass
+
+    def start_shortcut_command(self, shortcut_number: int):
+        pass
+
+    def apply_python_commands_filter(self):
+        pass
+
+    def set_python_command(self):
+        pass
+
+    def apply_mcu_commands_filter(self):
+        pass
+
+    def set_mcu_command(self):
+        pass
+
+    def load_python_command_list(self) -> list[str]:
+        return ["Command 1", "Command 2", "Command 3"]
+
+    def load_python_commands_filter_list(self) -> list[str]:
+        return ["-"]
+
+    def load_mcu_command_list(self) -> list[str]:
+        return ["Command 1", "Command 2", "Command 3"]
+
+    def load_mcu_commands_filter_list(self) -> list[str]:
+        return ["-"]
+
+    def open_commands_directory_window(self):
+        pass
+
+    def load_camera_list(self) -> list[str]:
+        return ["Camera 1", "Camera 2", "Camera 3"]
+
+    def load_camera_size_list(self) -> list[str]:
+        return [f"{320 * i}x{180 * i}" for i in range(1, 7)]
+
+    def connect_camera(self):
+        pass
+
+    def apply_camera_name(self):
+        pass
+
+    def apply_camera_fps(self):
+        pass
+
+    def apply_camera_size(self):
+        pass
+
+    def apply_camera_show_realtime(self):
+        pass
+
+    def apply_camera_show_matched(self):
+        pass
+
+    def apply_camera_show_guide(self):
+        pass
+
+    def save_screencapture(self):
+        pass
+
+    def open_screencapture_directory_window(self):
+        pass
+
+    def load_serial_port_list(self) -> list[str]:
+        return ["COM1", "COM2", "COM3"]
+
+    def load_serial_baud_rate_list(self) -> list[int]:
+        return [4800, 9600, 115200]
+
+    def load_serial_data_format_list(self) -> list[str]:
+        return ["Default", "Qingpi", "3DS Controller"]
+
+    def connect_serial_port(self):
+        pass
+
+    def disconnect_serial_port(self):
+        pass
+
+    def push_controller_button(self, button: str):
+        pass
+
+    def release_controller_button(self, button: str):
+        pass
+
+    def open_controller_window(self):
+        pass
+
+    def apply_controller_data_format(self):
+        pass
+
+    def open_software_controller_window(self):
+        pass
+
+    def apply_enabled_keyboard(self):
+        pass
+
+    def apply_enabled_lstick_mouse(self):
+        pass
+
+    def apply_enabled_rstick_mouse(self):
+        pass
+
+    def apply_enabled_pro_controller(self):
+        pass
+
+    def apply_enabled_record_pro_controller(self):
+        pass
+
+    def clear_log_outputs(self):
+        self.clear_log_output(output_id=1)
+        self.clear_log_output(output_id=2)
+
+    def clear_log_output(self, output_id: int):
+        pass
+
+    def apply_change_log_stdout(self):
+        pass
+
+    def adjust_log_outputs_size(self):
+        pass
+
+    def notify_windows(self):
+        pass
+
+    def notify_discord(self):
+        pass
+
+    def notify_windows_force(self):
+        pass
+
+    def notify_discord_force(self):
+        pass
+
+    def apply_enabled_notify_windows_when_command_started(self):
+        pass
+
+    def apply_enabled_notify_windows_when_command_ended(self):
+        pass
+
+    def apply_enabled_notify_discord_when_command_started(self):
+        pass
+
+    def apply_enabled_notify_discord_when_command_ended(self):
+        pass
+
+    def apply_widget_layout(self):
+        pass
+
+    def apply_outputs_visibility(self):
+        pass
+
+    def apply_software_controller_visibility(self):
+        pass
+
+    def apply_software_controller_position(self):
+        pass
+
+    def apply_confirm_buttons_position(self):
+        pass
--- packages/poke-controller/src/pokecontroller/app/model.py
+++ packages/poke-controller/src/pokecontroller/app/model.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from .state import PokeControllerAppState
 
@@ -12,193 +12,251 @@ class PokeControllerAppModel(ABC):
         return self._state
 
     # Command Functionalities
+    @abstractmethod
     def load_commands(self):
         pass
 
+    @abstractmethod
     def start_command(self):
         pass
 
+    @abstractmethod
     def stop_command(self):
         pass
 
+    @abstractmethod
     def pause_command(self):
         pass
 
     # FIXME: 必要か検証する
+    @abstractmethod
     def set_command_shortcut_number(self):
         pass
 
+    @abstractmethod
     def register_command_shortcut(self):
         pass
 
+    @abstractmethod
     def start_shortcut_command(self, shortcut_number: int):
         pass
 
+    @abstractmethod
     def apply_python_commands_filter(self):
         pass
 
     # FIXME: 必要か検証する
+    @abstractmethod
     def set_python_command(self):
         pass
 
+    @abstractmethod
     def apply_mcu_commands_filter(self):
         pass
 
     # FIXME: 必要か検証する
+    @abstractmethod
     def set_mcu_command(self):
         pass
 
+    @abstractmethod
     def load_python_command_list(self) -> list[str]:
-        return ["Command 1", "Command 2", "Command 3"]
+        pass
 
+    @abstractmethod
     def load_python_commands_filter_list(self) -> list[str]:
         return ["-"]
 
+    @abstractmethod
     def load_mcu_command_list(self) -> list[str]:
-        return ["Command 1", "Command 2", "Command 3"]
+        pass
 
+    @abstractmethod
     def load_mcu_commands_filter_list(self) -> list[str]:
-        return ["-"]
+        pass
 
+    @abstractmethod
     def open_commands_directory_window(self):
         pass
 
     # Camera Functionalities
+    @abstractmethod
     def load_camera_list(self) -> list[str]:
-        return ["Camera 1", "Camera 2", "Camera 3"]
+        pass
 
+    @abstractmethod
     def load_camera_size_list(self) -> list[str]:
-        return [f"{320 * i}x{180 * i}" for i in range(1, 7)]
+        pass
 
+    @abstractmethod
     def connect_camera(self):
         pass
 
     # FIXME: 必要か検証する
+    @abstractmethod
     def apply_camera_name(self):
         pass
 
     # FIXME: 必要か検証する
+    @abstractmethod
     def apply_camera_fps(self):
         pass
 
     # FIXME: 必要か検証する
+    @abstractmethod
     def apply_camera_size(self):
         pass
 
     # FIXME: 必要か検証する(多分いらない)
+    @abstractmethod
     def apply_camera_show_realtime(self):
         pass
 
+    @abstractmethod
     def apply_camera_show_matched(self):
         pass
 
+    @abstractmethod
     def apply_camera_show_guide(self):
         pass
 
+    @abstractmethod
     def save_screencapture(self):
         pass
 
+    @abstractmethod
     def open_screencapture_directory_window(self):
         pass
 
     # Serial Functionalities
+    @abstractmethod
     def load_serial_port_list(self) -> list[str]:
-        return ["COM1", "COM2", "COM3"]
+        pass
 
+    @abstractmethod
     def load_serial_baud_rate_list(self) -> list[int]:
-        return [4800, 9600, 115200]
+        pass
 
+    @abstractmethod
     def load_serial_data_format_list(self) -> list[str]:
-        return ["Default", "Qingpi", "3DS Controller"]
+        pass
 
+    @abstractmethod
     def connect_serial_port(self):
         pass
 
+    @abstractmethod
     def disconnect_serial_port(self):
         pass
 
     # Controller Functionalities
+    @abstractmethod
     def push_controller_button(self, button: str):
         pass
 
+    @abstractmethod
     def release_controller_button(self, button: str):
         pass
 
+    @abstractmethod
     def open_controller_window(self):
         pass
 
+    @abstractmethod
     def apply_controller_data_format(self):
         pass
 
+    @abstractmethod
     def open_software_controller_window(self):
         pass
 
+    @abstractmethod
     def apply_enabled_keyboard(self):
         pass
 
+    @abstractmethod
     def apply_enabled_lstick_mouse(self):
         pass
 
+    @abstractmethod
     def apply_enabled_rstick_mouse(self):
         pass
 
+    @abstractmethod
     def apply_enabled_pro_controller(self):
         pass
 
+    @abstractmethod
     def apply_enabled_record_pro_controller(self):
         pass
 
     # Logging Functionalities
+    @abstractmethod
     def clear_log_outputs(self):
-        self.clear_log_output(output_id=1)
-        self.clear_log_output(output_id=2)
+        pass
 
+    @abstractmethod
     def clear_log_output(self, output_id: int):
         pass
 
+    @abstractmethod
     def apply_change_log_stdout(self):
         pass
 
+    @abstractmethod
     def adjust_log_outputs_size(self):
         pass
 
     # Notification Functionalities
+    @abstractmethod
     def notify_windows(self):
         pass
 
+    @abstractmethod
     def notify_discord(self):
         pass
 
+    @abstractmethod
     def notify_windows_force(self):
         pass
 
+    @abstractmethod
     def notify_discord_force(self):
         pass
 
+    @abstractmethod
     def apply_enabled_notify_windows_when_command_started(self):
         pass
 
+    @abstractmethod
     def apply_enabled_notify_windows_when_command_ended(self):
         pass
 
+    @abstractmethod
     def apply_enabled_notify_discord_when_command_started(self):
         pass
 
+    @abstractmethod
     def apply_enabled_notify_discord_when_command_ended(self):
         pass
 
     # Widget Layout Functionalities
+    @abstractmethod
     def apply_widget_layout(self):
         pass
 
+    @abstractmethod
     def apply_outputs_visibility(self):
         pass
 
+    @abstractmethod
     def apply_software_controller_visibility(self):
         pass
 
+    @abstractmethod
     def apply_software_controller_position(self):
         pass
 
+    @abstractmethod
     def apply_confirm_buttons_position(self):
         pass
