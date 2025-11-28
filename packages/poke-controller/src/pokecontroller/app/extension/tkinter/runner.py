169e5e4cf501916d3354c4cadc6e2e07ffaef41e
carimatics <github.carimatics@gmail.com>
2025-11-28 23:33:41 +0900

    feat: integrate logging setup in runner and update log file paths in config

--- packages/poke-controller/src/pokecontroller/app/extension/tkinter/runner.py
+++ packages/poke-controller/src/pokecontroller/app/extension/tkinter/runner.py
@@ -1,9 +1,12 @@
+from ....core.logging import setup_logging
 from .app import PokeControllerExtensionApp
 from .values import literals as l
 from .windows import MainWindow
 
 
 def run_app() -> None:
+    setup_logging()
+
     app = PokeControllerExtensionApp()
     main_window = MainWindow(app)
     main_window.pack(expand=True, fill=l.BOTH)
