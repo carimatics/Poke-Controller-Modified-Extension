a12f235a5f3166fea8b7aecfd806b71002ecdd39
carimatics <github.carimatics@gmail.com>
2025-11-29 22:11:12 +0900

    refactor: extract dynamic import utilities to `utils.py` and simplify `CommandLoader` logic

--- apps/poke-controller-modified-extension/src/pokecontrollermodifiedextension/core/command/loader.py
+++ apps/poke-controller-modified-extension/src/pokecontrollermodifiedextension/core/command/loader.py
@@ -1,44 +1,36 @@
-import glob
 import importlib
 import logging
-import inspect
-import os
 import sys
 from os import PathLike
-from os.path import relpath
-from pathlib import Path
 from types import ModuleType
-from pokecontroller.core.path import join
+
+from ..utils import get_all_modules, get_module_names, get_classes_in_module
 
 logger = logging.getLogger(__name__)
 
 
 class CommandLoader[T]:
     def __init__(self, base_path: str | PathLike[T], base_class: type[T]) -> None:
-        self.path: Path = Path(base_path)
+        self.path: str = base_path
         self.base_type: type[T] = base_class
         self.modules: list[ModuleType] = []
 
     def load(self) -> list[type[T]]:
         """指定ディレクトリからCommandクラスを動的に読み込む"""
-        if not self.path.exists():
-            raise FileNotFoundError(f"Commands directory not found:  {self.path}")
-
         if self.modules:
-            module_names = self._load_module_names()
-            self.modules = self._load_modules(module_names)
+            self.modules = get_all_modules(self.path)
         return self._get_command_classes()
 
     def reload(self) -> list[type[T]]:
         current_modules = {mod.__name__: mod for mod in self.modules}
 
         current_module_names = set(current_modules.keys())
-        renewed_module_names = set(self._load_module_names())
+        renewed_module_names = set(get_module_names(self.path))
 
         # Load only not loaded modules
         unloaded_module_names = list(renewed_module_names - current_module_names)
         if unloaded_module_names:
-            self.modules.extend(self._load_modules(unloaded_module_names))
+            self.modules.extend(get_all_modules(self.path, unloaded_module_names))
 
         # Reload commands except deleted ones
         old_module_names = list(renewed_module_names & current_module_names)
@@ -55,39 +47,20 @@ class CommandLoader[T]:
         # return command class types
         return self._get_command_classes()
 
-    def getCommandClasses(self):
+    # for compatibility
+    def getCommandClasses(self): # noqa
         return self._get_command_classes()
-
-    # noinspection PyMethodMayBeStatic
-    def _load_modules(self, module_names: list[str]) -> list[ModuleType]:
-        """self.path内の.pyファイルをモジュールとして読み込んで返す"""
-        return [
-            importlib.import_module(name)
-            for name in module_names
-        ]
-
-    def _load_module_names(self) -> list[str]:
-        """self.path内の.pyファイルをモジュール名を返す"""
-        # globで検索するパスの構築
-        search_path = join(str(self.path), "**", "*.py")
-
-        return [
-            # 相対パスから拡張子(.py)を除いてファイルの区切り文字をドット(.)に変換する
-            (relpath(py_file)[:-3]).replace(os.sep, ".")
-            for py_file in glob.glob(search_path, recursive=True)
-        ]
 
     def _get_command_classes(self) -> list[type[T]]:
         """self.modulesからbase_typeクラスのサブクラスを取得する"""
         classes = []
         for mod in self.modules:
             class_list = [
-                obj
-                for _, obj in inspect.getmembers(mod, inspect.isclass)
+                c for c in get_classes_in_module(mod)
                 if (
-                    issubclass(obj, self.base_type) and
-                    obj is not self.base_type and
-                    hasattr(obj, "NAME") and obj.NAME
+                    issubclass(c, self.base_type) and
+                    c is not self.base_type and
+                    hasattr(c, "NAME") and c.NAME
                 )
             ]
 
@@ -96,6 +69,7 @@ class CommandLoader[T]:
                 dir_name = "/".join(mod.__name__.split(".")[2:])
                 dir_tags = ["@" + t for t in mod.__name__.split(".")[2:-1]]
 
+                # add tags of directory name
                 if hasattr(c, "TAGS"):
                     if isinstance(c.TAGS, list):
                         logger.debug(f"TAGS name add: {dir_tags}")
---
+++ apps/poke-controller-modified-extension/src/pokecontrollermodifiedextension/core/utils.py
@@ -0,0 +1,52 @@
+from types import ModuleType
+import glob
+import importlib
+import inspect
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+
+def ospath(path: str) -> str:
+    return path.replace("/", os.sep)
+
+
+def browse_file_names(
+    path: str = ".",
+    ext: str | None = "",
+    recursive: bool | None = True,
+    name_only: bool | None = True,
+) -> list[str]:
+    search_path = os.path.join(path, "**") if recursive else path
+    search_path = os.path.join(search_path, "*" + ext)
+
+    if name_only:
+        return [
+            os.path.relpath(f, path)
+            for f in glob.glob(search_path, recursive=recursive)
+        ]
+    else:
+        return glob.glob(search_path, recursive=recursive)
+
+
+def get_classes_in_module(module: ModuleType):
+    return [
+        obj for _, obj in inspect.getmembers(module, inspect.isclass)
+    ]
+
+
+def get_module_names(base_path: str) -> list[str]:
+    return [
+        # 拡張子(.py)を除いてファイルの区切り文字をドット(.)に置き換える
+        name[:-3].replace(os.sep, ".")
+        for name in browse_file_names(path=base_path, ext=".py", name_only=False)
+    ]
+
+
+def get_all_modules(base_path: str, mod_names: list[str] | None = None):
+    modules = []
+    for name in get_module_names(base_path) if mod_names is None else mod_names:
+        logger.debug(f"Import module: {name}")
+        modules.append(importlib.import_module(name))
+    return modules
