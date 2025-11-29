f5a117d34b88ea17f5fd86c8f98476dc2afd871b
carimatics <github.carimatics@gmail.com>
2025-11-30 02:20:25 +0900

    refactor: introduce `Camera` module and enhance logging, modularity, and functionality in camera handling

---
+++ apps/poke-controller-modified-extension/src/pokecontrollermodifiedextension/core/camera.py
@@ -0,0 +1,138 @@
+import logging
+from contextlib import contextmanager
+from typing import Sequence, Generator
+from pokecontroller.core import (
+    image as imagelib,
+    camera as cameralib,
+    path as pathlib,
+    datetime as datetimelib,
+)
+from pokecontroller.core.image import utils as imagelib_utils
+
+logger = logging.getLogger(__name__)
+
+
+def imwrite(
+    filename: str,
+    img: imagelib.RawImage,
+    params: Sequence[int] = None) -> bool:
+    try:
+        return imagelib.write(img, filename, params)
+    except Exception as e:
+        logger.error(f"Image Write Error: {e}")
+        return False
+
+
+CAPTURE_DIR = "Captures"
+
+
+def _get_save_filespec(filename: str) -> str:
+    """
+    画像ファイルの保存パスを取得する。
+
+    入力が絶対パスの場合は、`CAPTURE_DIR`につなげずに返す。
+
+    Args:
+        filename (str): 保存名／保存パス
+
+    Returns:
+        str: _description_
+    """
+    if pathlib.is_absolute(filename):
+        return filename
+    else:
+        return pathlib.to_absolute(pathlib.join(CAPTURE_DIR, filename))
+
+
+class Camera:
+    def __init__(self, fps: int = 45):
+        self.camera = cameralib.Camera(fps=fps, frame_size=(1280, 720))
+        self.image_bgr = None
+        # self.capture_size = (1920, 1080)
+        self.capture_dir = "Captures"
+
+    @property
+    def fps(self):
+        return self.camera.fps
+
+    @fps.setter
+    def fps(self, fps: int):
+        self.camera.fps = fps
+
+    @property
+    def capture_size(self):
+        return self.camera.frame_size
+
+    @capture_size.setter
+    def capture_size(self, size: tuple[int, int]):
+        self.camera.frame_size = size
+
+    def openCamera(self, cameraId: int):  # noqa
+        self.camera.open(camera_id=cameraId)
+
+    def isOpened(self):  # noqa
+        return self.camera.is_opened
+
+    def readFrame(self) -> imagelib.RawImage | None:  # noqa
+        _, self.image_bgr = self.camera.read()
+        return self.image_bgr
+
+    def saveCapture(  # noqa
+        self,
+        filename: str = None,
+        crop: int = None,
+        crop_ax: list[int] = None,
+        img: imagelib.RawImage = None,
+    ):
+        if crop_ax is None:
+            c = [0, 0, self.capture_size[0], self.capture_size[1]]
+        else:
+            c = crop_ax
+
+        crop_fmt = int(crop) if crop is not None else None
+        if crop_fmt is None:
+            image = self.image_bgr
+        elif crop_fmt:
+            imagelib_utils.convert_to_default(c, crop_fmt)
+            args = imagelib.ImageCropArgs(
+                ys=c[0],
+                ye=c[1],
+                xs=c[2],
+                xe=c[3],
+            )
+            image = imagelib.crop(self.image_bgr, args)
+        elif img is not None:
+            image = img
+        else:
+            image = self.image_bgr
+
+        if not filename:
+            fn = f"{datetimelib.format_datetime()}.png"
+        else:
+            fn = filename + ".png"
+        save_path = _get_save_filespec(fn)
+
+        if not pathlib.exists_directory((save_dir := pathlib.directory_name(save_path))):
+            # 保存先ディレクトリが存在しないか、同名のファイルが存在する場合（existsはファイルとフォルダを区別しない）
+            pathlib.make_directory(save_dir)
+            logger.debug("Created Capture folder")
+
+        try:
+            imwrite(save_path, image)
+            logger.debug(f"Capture succeeded: {save_path}")
+        except Exception as e:
+            logger.error(f"Capture Failed :{e}")
+
+    def destroy(self):
+        self.camera.close()
+
+
+@contextmanager
+def use_camera(
+    fps: int = 45,
+) -> Generator[Camera, None, None]:
+    camera = Camera(fps=fps)
+    try:
+        yield camera
+    finally:
+        camera.destroy()
--- packages/poke-controller/src/pokecontroller/core/camera/camera.py
+++ packages/poke-controller/src/pokecontroller/core/camera/camera.py
@@ -1,8 +1,11 @@
-import os
+import logging
 
 import cv2
 
 from ..image import RawImage
+from ..platform import is_windows
+
+logger = logging.getLogger(__name__)
 
 
 class Camera:
@@ -20,8 +23,11 @@ class Camera:
     @property
     def is_opened(self) -> bool:
         if self._video_capture is None:
+            logger.debug("Camera is not opened")
             return False
-        return self._video_capture.isOpened()
+        is_opened = self._video_capture.isOpened()
+        logger.debug(f"Camera is_opened: {is_opened}")
+        return is_opened
 
     @property
     def frame_size(self) -> tuple[int, int]:
@@ -30,12 +36,7 @@ class Camera:
     @frame_size.setter
     def frame_size(self, size: tuple[int, int]) -> None:
         self._frame_size = size
-        if (vc := self._video_capture) is None:
-            return
-        if vc.isOpened():
-            width, height = size
-            vc.set(cv2.CAP_PROP_FRAME_WIDTH, float(width))
-            vc.set(cv2.CAP_PROP_FRAME_HEIGHT, float(height))
+        self._set_video_capture_frame_size(size)
 
     @property
     def fps(self) -> int:
@@ -44,10 +45,7 @@ class Camera:
     @fps.setter
     def fps(self, fps: int) -> None:
         self._fps = fps
-        if (vc := self._video_capture) is None:
-            return
-        if vc.isOpened():
-            vc.set(cv2.CAP_PROP_FPS, float(fps))
+        self._set_video_capture_fps(fps)
 
     @property
     def frame(self) -> RawImage | None:
@@ -56,30 +54,56 @@ class Camera:
     def open(self, camera_id: int) -> None:
         self.close()
 
-        match os.name:
-            case "nt":  # Windows
-                self._video_capture = cv2.VideoCapture(camera_id, cv2.CAP_DSHOW)
-            case _:
-                self._video_capture = cv2.VideoCapture(camera_id)
+        if is_windows():
+            logger.debug("NT OS")
+            self._video_capture = cv2.VideoCapture(camera_id, cv2.CAP_DSHOW)
+        else:
+            logger.debug("Not NT OS")
+            self._video_capture = cv2.VideoCapture(camera_id)
 
         if not self.is_opened:
+            logger.error(f"Camera ID {camera_id} cannot open.")
             return
 
-        self.frame_size = self._frame_size
-        self.fps = self._fps
+        logger.debug(f"Camera ID {camera_id} opened successfully.")
+        self._set_video_capture_frame_size(self._frame_size)
+        self._set_video_capture_fps(self._fps)
 
     def close(self) -> None:
         if (vc := self._video_capture) is None:
+            logger.debug("Camera is not opened")
             return
         if vc.isOpened():
+            logger.debug("Closing camera")
             vc.release()
         self._video_capture = None
+        logging.debug("Camera closed")
 
-    def read(self) -> bool:
+    def read(self) -> tuple[bool, RawImage | None]:
         if (vc := self._video_capture) is None:
-            return False
+            return False, None
 
         if vc.isOpened():
             success, self._frame = vc.read()
-            return success
-        return False
+            return success, self._frame
+        return False, None
+
+    def _set_video_capture_frame_size(self, size: tuple[int, int]) -> None:
+        logger.debug(f"Setting frame size to {size}")
+        if (vc := self._video_capture) is None:
+            logger.debug("Camera is not opened")
+            return
+        if vc.isOpened():
+            width, height = size
+            vc.set(cv2.CAP_PROP_FRAME_WIDTH, float(width))
+            vc.set(cv2.CAP_PROP_FRAME_HEIGHT, float(height))
+        logger.debug("Frame size set successfully")
+
+    def _set_video_capture_fps(self, fps: int) -> None:
+        logger.debug(f"Setting fps to {fps}")
+        if (vc := self._video_capture) is None:
+            logger.debug("Camera is not opened")
+            return
+        if vc.isOpened():
+            vc.set(cv2.CAP_PROP_FPS, float(fps))
+        logger.debug("FPS set successfully")
