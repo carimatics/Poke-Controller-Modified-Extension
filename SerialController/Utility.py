540adaf428d3db9bfab726aaaf8320d000a0ad5c
carimatics <github.carimatics@gmail.com>
2025-11-30 13:12:11 +0900

    refactor: simplify `Utility.py` by replacing custom compatibility functions with direct imports from `utils`

--- SerialController/Utility.py
+++ SerialController/Utility.py
@@ -1,38 +1,8 @@
-from mypy.moduleinspect import ModuleType
-
-from pokecontrollermodifiedextension.core import utils
-
-
 # for compatibility
-def ospath(path: str):  # noqa
-    return utils.ospath(path=path)
-
-
-# for compatibility
-def browseFileNames(  # noqa
-    path: str = ".",
-    ext: str = "",
-    recursive: bool = True,
-    name_only: bool = True,
-):
-    return utils.browse_file_names(
-        path=path,
-        ext=ext,
-        recursive=recursive,
-        name_only=name_only,
-    )
-
-
-# for compatibility
-def getClassesInModule(module: ModuleType):  # noqa
-    return utils.get_classes_in_module(module=module)
-
-
-# for compatibility
-def getModuleNames(base_path: str):  # noqa
-    return utils.get_module_names(base_path=base_path)
-
-
-# for compatibility
-def importAllModules(base_path: str, mod_names: list[str] | None = None):  # noqa
-    return utils.get_all_modules(base_path=base_path, mod_names=mod_names)
+from pokecontrollermodifiedextension.core.utils import (  # noqa
+    ospath as ospath,  # noqa
+    browse_file_names as browseFileNames,  # noqa
+    get_classes_in_module as getClassesInModule,  # noqa
+    get_module_names as getModuleNames,  # noqa
+    get_all_modules as importAllModules,  # noqa
+)
