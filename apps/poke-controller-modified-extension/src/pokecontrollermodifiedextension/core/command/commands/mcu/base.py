edfb71cd7e522374fac67edadcc2253e6218a8a5
carimatics <github.carimatics@gmail.com>
2025-12-02 01:44:26 +0900

    refactor: update `McuCommand` implementation with type annotations and `ABC`; update dependencies and cleanup `ruff.toml`/`mypy.ini` configuration

--- apps/poke-controller-modified-extension/src/pokecontrollermodifiedextension/core/command/commands/mcu/base.py
+++ apps/poke-controller-modified-extension/src/pokecontrollermodifiedextension/core/command/commands/mcu/base.py
@@ -1,25 +1,27 @@
+from abc import ABC
+from collections.abc import Callable
+
+from ... import Sender
 from ..base import Command
 
 
-class McuCommand(Command):
+class McuCommand(Command, ABC):
     def __init__(self, sync_name: str):
         super().__init__()
         self.sync_name = sync_name
-        self.postProcess = None
+        self.postProcess: Callable[[], None] | None = None
 
-    def start(self, ser, postProcess=None):
+    def start(
+        self,
+        ser: Sender,
+        postProcess: Callable[[], None] | None = None,  # noqa
+    ) -> None:
         ser.writeRow(self.sync_name)
         self.isRunning = True
         self.postProcess = postProcess
 
-    def end(self, ser):
+    def end(self, ser: Sender) -> None:
         ser.writeRow("end")
         self.isRunning = False
-        if self.postProcess:
-            self.postProcess()
-
-    def finish(self):
-        pass
-
-    def checkIfAlive(self):
-        pass
+        if (p := self.postProcess) is not None:
+            p()
