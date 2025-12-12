2cc225215c9cf4041d1cdc7dd5356d8b74105c33
carimatics <github.carimatics@gmail.com>
2025-12-12 12:05:31 +0900

    refactor: enhance `configure_style` method for improved flexibility
    - Added optional `variant` and `size` parameters with default fallback to previous state.
    - Updated `_construct_style` and internal state management for consistency.

--- apps/poke-controller-modified-extension/src/pokecontrollermodifiedextension/widgets/button.py
+++ apps/poke-controller-modified-extension/src/pokecontrollermodifiedextension/widgets/button.py
@@ -18,14 +18,30 @@ class Button(ttk.Button):
         self._pokecon_style = self._construct_style(variant, size)
         kwargs["style"] = self._pokecon_style
         super().__init__(master, **kwargs)
+        self._pokecon_size = size
+        self._pokecon_variant = variant
 
-    def configure_style(self, variant: VariantType, size: SizeType) -> None:
+    def configure_style(
+        self,
+        *,
+        variant: VariantType | None = None,
+        size: SizeType | None = None,
+    ) -> None:
+        if variant is None:
+            variant = self._pokecon_variant
+        if size is None:
+            size = self._pokecon_size
         style = self._construct_style(variant, size)
         self.configure(style=style)
         self._pokecon_style = style
+        self._pokecon_size = size
+        self._pokecon_variant = variant
 
     @staticmethod
-    def _construct_style(variant: VariantType, size: SizeType) -> str:
+    def _construct_style(
+        variant: VariantType,
+        size: SizeType,
+    ) -> str:
         styles = ["PokeController"]
         if size != "md":
             styles.append(size.capitalize())
