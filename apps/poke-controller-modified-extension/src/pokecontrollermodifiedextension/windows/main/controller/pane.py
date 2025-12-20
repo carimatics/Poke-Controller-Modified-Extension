ca5eef5966f4a7a227ebd2fa3576b39397a8ee5c
carimatics <github.carimatics@gmail.com>
2025-12-20 18:15:05 +0900

    feat: add button configuration and controller interaction to `ControllerPane`
    - Introduced `BUTTON_CONFIGS` dictionary for dynamic button mapping and actions in `ControllerPane`.
    - Integrated controller interactions using `SwitchController` for button press/release events with state updates.
    - Added logging for button press/release actions to improve debugging.
    - Refactored UI logic to use `BUTTON_CONFIGS` for button text and actions.

--- apps/poke-controller-modified-extension/src/pokecontrollermodifiedextension/windows/main/controller/pane.py
+++ apps/poke-controller-modified-extension/src/pokecontrollermodifiedextension/windows/main/controller/pane.py
@@ -1,9 +1,13 @@
+import logging
 import platform
 import tkinter as tk
 from typing import Any
 
+from pokecontroller.core.controller import switch
+
 from .... import widgets
 from ....model import get_app_model
+from ....resources import get_app_resources
 from ....widgets.app import AppFrame
 
 A = "A"
@@ -43,11 +47,174 @@ BUTTON_COLORS = {
     "fg": "#FFFFFF" if platform.system() == "Windows" else None,
 }
 
+logger = logging.getLogger(__name__)
+
 
 class ControllerPane(AppFrame):
+    BUTTON_CONFIGS: dict[str, dict[str, Any]] = {
+        A: {
+            "text": "A",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.A],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.A],
+            ),
+        },
+        B: {
+            "text": "B",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.B],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.B],
+            ),
+        },
+        X: {
+            "text": "X",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.X],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.X],
+            ),
+        },
+        Y: {
+            "text": "Y",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.Y],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.Y],
+            ),
+        },
+        L: {
+            "text": "L",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.L],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.L],
+            ),
+        },
+        R: {
+            "text": "R",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.R],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.R],
+            ),
+        },
+        ZL: {
+            "text": "ZL",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.ZL],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.ZL],
+            ),
+        },
+        ZR: {
+            "text": "ZR",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.ZR],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.ZR],
+            ),
+        },
+        LC: {
+            "text": "L-C",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.LS],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.LS],
+            ),
+        },
+        RC: {
+            "text": "R-C",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.RS],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.RS],
+            ),
+        },
+        LSR: {
+            "text": "→",
+            "pressed": lambda self: self._controller.lstick.tilt_by_preset(
+                tilt=switch.SwitchStickTilt.RIGHT,
+            ),
+            "released": lambda self: self._controller.lstick.to_neutral(),
+        },
+        LSU: {
+            "text": "↑",
+            "pressed": lambda self: self._controller.lstick.tilt_by_preset(
+                tilt=switch.SwitchStickTilt.TOP,
+            ),
+            "released": lambda self: self._controller.lstick.to_neutral(),
+        },
+        LSL: {
+            "text": "←",
+            "pressed": lambda self: self._controller.lstick.tilt_by_preset(
+                tilt=switch.SwitchStickTilt.LEFT,
+            ),
+            "released": lambda self: self._controller.lstick.to_neutral(),
+        },
+        LSD: {
+            "text": "↓",
+            "pressed": lambda self: self._controller.lstick.tilt_by_preset(
+                tilt=switch.SwitchStickTilt.BOTTOM,
+            ),
+            "released": lambda self: self._controller.lstick.to_neutral(),
+        },
+        CAP: {
+            "text": "CAP",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.CAPTURE],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.CAPTURE],
+            ),
+        },
+        HOME: {
+            "text": "HOME",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.HOME],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.HOME],
+            ),
+        },
+        MIN: {
+            "text": "-",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.MINUS],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.MINUS],
+            ),
+        },
+        PLUS: {
+            "text": "+",
+            "pressed": lambda self: self._controller.buttons.push(
+                buttons=[switch.SwitchButton.PLUS],
+            ),
+            "released": lambda self: self._controller.buttons.release(
+                buttons=[switch.SwitchButton.PLUS],
+            ),
+        },
+    }
+
     def __init__(self, master: tk.Misc, *args: Any, **kwargs: Any) -> None:
         super().__init__(master, *args, **kwargs)
+
+        resources = get_app_resources()
+        self._controller = switch.SwitchController(serial=resources.serial)
         self._app_model = get_app_model()
+
         self.build_ui()
 
     def build_ui(self) -> None:
@@ -82,9 +249,10 @@ class ControllerPane(AppFrame):
                 if button is None:
                     continue
 
+                config = self.BUTTON_CONFIGS[button]
                 b = tk.Button(
                     frame,
-                    text=button,
+                    text=config["text"],
                     width=4,
                 )
                 if bg := BUTTON_COLORS.get("bg"):
@@ -94,7 +262,7 @@ class ControllerPane(AppFrame):
 
                 def on_pushed(_: tk.Event, btn: str | None = button) -> None:
                     if btn is not None:
-                        self._on_button_pushed(btn)
+                        self._on_button_pressed(btn)
 
                 def on_released(_: tk.Event, btn: str | None = button) -> None:
                     if btn is not None:
@@ -104,8 +272,12 @@ class ControllerPane(AppFrame):
                 b.bind("<ButtonRelease>", func=on_released, add="")
                 b.grid(row=row, column=column, padx=2, pady=2, sticky=tk.NSEW)
 
-    def _on_button_pushed(self, button: str) -> None:
-        self._app_model.push_controller_button(button)
+    def _on_button_pressed(self, button: str) -> None:
+        logger.info(f"Button {button} pressed")
+        self.BUTTON_CONFIGS[button]["pressed"](self)
+        self._controller.send_state()
 
     def _on_button_released(self, button: str) -> None:
-        self._app_model.release_controller_button(button)
+        logger.info(f"Button {button} released")
+        self.BUTTON_CONFIGS[button]["released"](self)
+        self._controller.send_state()
